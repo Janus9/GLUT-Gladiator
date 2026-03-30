f011fe1fe85a1c20b5589856b1f79cc8238f9a0c
Frank Bernal <frankbernal@mail.fresnostate.edu>
2026-03-30 13:06:09 -0700

    I hit save and my linter reformatted all the spacing.

--- src/_inputs.cpp
+++ src/_inputs.cpp
@@ -2,84 +2,80 @@
 
 _inputs::_inputs()
 {
-    //ctor
-    isRotate = false; //Not currently rotating
-    isTranslate = false; //Not currently translating
+    // ctor
+    isRotate = false;    // Not currently rotating
+    isTranslate = false; // Not currently translating
 }
 
 _inputs::~_inputs()
 {
-    //dtor
+    // dtor
 }
 
 void _inputs::keyPressed()
 {
-    switch(wParam)//switch case to check different key inputs
+    switch (wParam) // switch case to check different key inputs
     {
-        case VK_LEFT:
-        //left key
+    case VK_LEFT:
+        // left key
         break;
-        case VK_RIGHT:
-        //right key
+    case VK_RIGHT:
+        // right key
         break;
-        case VK_UP:
-        //up key
+    case VK_UP:
+        // up key
         break;
-        case VK_DOWN:
-        //left key
+    case VK_DOWN:
+        // left key
         break;
     }
 }
 
 void _inputs::keyUp()
 {
-    switch(wParam)
+    switch (wParam)
     {
-        default: break; //The default key is currently at break
+    default:
+        break; // The default key is currently at break
     }
 }
 
 void _inputs::mouseEventDown(double x, double y)
 {
-    prevMx = x; //The double value for x is taken in and stored as that value
-    prevMy = y; //The double value for y is taken in and stored as that value
+    prevMx = x; // The double value for x is taken in and stored as that value
+    prevMy = y; // The double value for y is taken in and stored as that value
 
-    switch(wParam)
+    switch (wParam)
     {
     case MK_LBUTTON:
-        isRotate = true; //the Left mouse button being held means the object will rotate
+        isRotate = true; // the Left mouse button being held means the object will rotate
         break;
     case MK_RBUTTON:
-        isTranslate = true; //the right mouse button being held means the object will translate
+        isTranslate = true; // the right mouse button being held means the object will translate
         break;
-    case MK_MBUTTON: //the middle button can be set to a value
+    case MK_MBUTTON: // the middle button can be set to a value
         break;
-    default: //The default is currently set to nothing yet can also be set
+    default: // The default is currently set to nothing yet can also be set
         break;
-
     }
 }
 
 void _inputs::mouseEvenUp()
 {
-    isRotate = false; //The mouse will stop rotating the object
-    isTranslate = false; //The mouse will stop translating the object
+    isRotate = false;    // The mouse will stop rotating the object
+    isTranslate = false; // The mouse will stop translating the object
 }
 
 void _inputs::mouseMove(double, double)
 {
-    if(isRotate)
+    if (isRotate)
     {
-
     }
-    if(isTranslate)
+    if (isTranslate)
     {
-
     }
 }
 
 void _inputs::mouseWheel(double delta)
 {
-
 }
-
