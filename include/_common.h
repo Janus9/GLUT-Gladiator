77d71535781acc2ca75f284596432d1d623a4eb3
Joshua Bayt <joshua.bayt@gmail.com>
2026-06-13 11:30:20 -0700

    Changed game version to 0.46

--- include/_common.h
+++ include/_common.h
@@ -7,7 +7,7 @@
 #define GRAVITY 9.81
 
 // Versions //
-#define GAME_VERSION 0.455          // Version of the game itself
+#define GAME_VERSION 0.46           // Version of the game itself
 #define WORLD_SAVE_VERSION 2        // Version of the world save system
 
 // INCLUDES //
