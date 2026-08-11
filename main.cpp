23bb39b4fbe9b7674edd74d80625d30d7264b27d
joshua.bayt@gmail.com <joshua.bayt@gmail.com>
2026-08-11 11:12:37 -0700

    Fixed the menu stopping game audio

--- main.cpp
+++ main.cpp
@@ -304,12 +304,12 @@ int main([[maybe_unused]] int argc,[[maybe_unused]] char *argv[])
 							if (menuManager->getLoadedPage() == menu::PAGE_GAME) {
 								// In game - pause
 								SDL_LogInfo(LOG_MAIN, "Pause game event");
+								soundEngine->stopAllBackgroundSounds();
 								menuManager->loadPage(menu::PAGE_PAUSE);
 							} else {
 								// In menu - unpause (if game loaded)
 								if (gameScene->isInitialized()) {
 									SDL_LogInfo(LOG_MAIN, "Unpause game event");
-									soundEngine->stopAllBackgroundSounds();
 									menuManager->loadPage(menu::PAGE_GAME);
 								}
 							}
